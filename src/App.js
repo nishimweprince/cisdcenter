91434b78fbccf6c1b55697db031e30647c3901ad
Nishimwe Prince <princeelysee@gmail.com>
2023-03-07 23:39:16 +0200

    Initial commit
    - Removed boiler plate.
    - Awaiting to receive credits from Azure before I start deploying.
    
    -- Will start building components tomorrow.

--- src/App.js
+++ src/App.js
@@ -4,20 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>Welcome to the very first home page of CISD's new website</h1>
+
+        </header>
+
     </div>
   );
 }
